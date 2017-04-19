95cfe4b170a3b6cda2642190f8d1288b297e8bd7
Duncan Henderson <duncanroberthenderson@gmail.com>
2017-04-19 16:29:01 +0100

    added PlayersTest to test team, name and rank

--- app/src/test/java/example/codeclan/com/topbballplayerslist/PlayersTest.java
+++ app/src/test/java/example/codeclan/com/topbballplayerslist/PlayersTest.java
@@ -1,8 +1,38 @@
 package example.codeclan.com.topbballplayerslist;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 /**
  * Created by user on 19/04/2017.
  */
 
 public class PlayersTest {
+
+    Players players;
+
+    @Before
+    public void before() {
+        players = new Players(21, "Bob Pettit", "St.Louis Hawks");
+    }
+
+    @Test
+    public void getRankingTest() {
+        assertEquals((Integer)21, players.getRanking());
+
+    }
+
+    @Test
+    public void getNameTest() {
+        assertEquals("Bob Pettit", players.getName());
+
+    }
+
+    @Test
+    public void getTeamTest() {
+        assertEquals("St.Louis Hawks", players.getTeam());
+
+    }
 }

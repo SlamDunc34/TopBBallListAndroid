37b0e11876484e3e7504eaa534c2dd6ce622f46d
Duncan Henderson <duncanroberthenderson@gmail.com>
2017-04-19 15:45:45 +0100

    created players java class

---
+++ app/src/main/java/example/codeclan/com/topbballplayerslist/Players.java
@@ -0,0 +1,32 @@
+package example.codeclan.com.topbballplayerslist;
+
+/**
+ * Created by user on 19/04/2017.
+ */
+
+public class Players {
+
+    private int ranking;
+    private String name;
+    private String team;
+
+    public Players(Integer ranking, String name, String team) {
+        this.ranking = ranking;
+        this.name = name;
+        this.team = team;
+    }
+
+    public Integer getRanking() {
+        return ranking;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getTeam() {
+        return team;
+    }
+
+}
+}
